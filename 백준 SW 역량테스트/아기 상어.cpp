0d60e4d775e54cad0bf5022e9c0d8df9d089c726
inpyeong <58331907+inpyeong@users.noreply.github.com>
2021-07-09 15:03:39 +0900

    Update 아기 상어.cpp

--- 백준 SW 역량테스트/아기 상어.cpp
+++ 백준 SW 역량테스트/아기 상어.cpp
@@ -1,132 +1,131 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <cstring>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
-const int MAX = 20;
+const int MAX = 20, D[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};
 
 struct Shark {
-	int y, x;
-	int size;
-	int eatCnt;
-	int time;
-}s;
+  int y, x;
+  int size;
+  int eatCnt;
+  int time;
+}Shark;
 
 struct Fish {
-	int y, x;
-	int dist;
+  int y, x;
+  int dist;
 };
 
-int n;
-int m[MAX][MAX];
-bool discovered[MAX][MAX];
-vector<Fish> v;
-
-int dy[] = { -1, 1,  0, 0 };
-int dx[] = {  0, 0, -1, 1 };
+int N;
+int Board[MAX][MAX];
+queue<pair<pair<int, int>, int> > Q;
+bool Discovered[MAX][MAX];
+vector<Fish> Fishes;
 
 void input() {
-	cin >> n;
-	for(int i = 0; i < n; ++i) 
-		for(int j = 0; j < n; ++j) {
-			cin >> m[i][j];
-			if(m[i][j] == 9) {
-				s.y = i;
-				s.x = j;
-				s.size = 2;
-				s.eatCnt = 0;
-				s.time = 0;
-			}
-		}
+  cin >> N;
+  for(int i = 0; i < N; ++i)
+    for(int j = 0; j < N; ++j) {
+      cin >> Board[i][j];
+      if(Board[i][j] == 9) {
+        Shark.y = i;
+        Shark.x = j;
+        Shark.size = 2;
+        Shark.eatCnt = 0;
+        Shark.time = 0;
+      }
+    }
 }
 
 void init() {
-	v.clear();
-	memset(discovered, false, sizeof(discovered));
+  Fishes.clear();
+  while(!Q.empty()) Q.pop();
+  memset(Discovered, false, sizeof(Discovered));
 }
 
-void bfs(int a, int b) {
-	queue<pair<pair<int, int>, int> > q;
-	q.push(make_pair(make_pair(a, b), 0));
-	discovered[a][b] = true;
-	while(!q.empty()) {
-		int y = q.front().first.first;
-		int x = q.front().first.second;
-		int dist = q.front().second;
-		q.pop();
-		for(int i = 0; i < 4; ++i) {
-			int ny = y + dy[i];
-			int nx = x + dx[i];
-			if(ny < 0 || ny >= n || nx < 0 || nx >= n)
-				continue;
-			if(discovered[ny][nx])
-				continue;
-			if(m[ny][nx] == 0 || m[ny][nx] == s.size) {
-				discovered[ny][nx] = true;
-				q.push(make_pair(make_pair(ny, nx), dist+1));
-			}
-			else if(m[ny][nx] < s.size) {
-				Fish temp;
-				temp.y = ny;
-				temp.x = nx;
-				temp.dist = dist+1;
-				v.push_back(temp);
-				discovered[ny][nx] = true;
-				q.push(make_pair(make_pair(ny, nx), dist+1));
-			}
-		}
-	}
+void bfs() {
+  Q.push(make_pair(make_pair(Shark.y, Shark.x), 0));
+  Discovered[Shark.y][Shark.x] = true;
+  while(!Q.empty()) {
+    int y = Q.front().first.first;
+    int x = Q.front().first.second;
+    int dist = Q.front().second;
+    Q.pop();
+    for(int i = 0; i < 4; ++i) {
+      int ny = y + D[i][0];
+      int nx = x + D[i][1];
+      if(ny < 0 || nx < 0 || ny >= N || nx >= N) continue;
+      if(Discovered[ny][nx]) continue;
+      if(Board[ny][nx] == 0 || Board[ny][nx] == Shark.size) {
+        Discovered[ny][nx] = true;
+        Q.push(make_pair(make_pair(ny, nx), dist+1));
+      } 
+      else if(Board[ny][nx] < Shark.size) {
+        Fish f;
+        f.y = ny;
+        f.x = nx;
+        f.dist = dist+1;
+        Fishes.push_back(f);
+        Discovered[ny][nx] = true;
+        Q.push(make_pair(make_pair(ny, nx), dist+1));
+      }
+    }
+  }
 }
 
 void eatFish() {
-	m[v.front().y][v.front().x] = 9;
-	m[s.y][s.x] = 0;
-	s.y = v.front().y;
-	s.x = v.front().x;
-	s.eatCnt++;
-	s.time += v.front().dist;
-	if(s.eatCnt == s.size) {
-		s.eatCnt = 0;
-		s.size++;
-	}
+  int y = Fishes.front().y;
+  int x = Fishes.front().x;
+  int dist = Fishes.front().dist;
+
+  Board[y][x] = 9;
+  Board[Shark.y][Shark.x] = 0;
+
+  Shark.y = y;
+  Shark.x = x;
+  Shark.eatCnt++;
+  Shark.time += dist;
+  if(Shark.eatCnt == Shark.size) {
+    Shark.size++;
+    Shark.eatCnt = 0;
+  }
 }
 
-bool cmp(Fish a, Fish b) {
-	if(a.dist == b.dist) {
-		if(a.y == b.y) {
-			return a.x < b.x;
-		}
-		return a.y < b.y;
-	}
-	return a.dist < b.dist;
+bool cmp(Fish& a, Fish& b) {
+  if(a.dist == b.dist) {
+    if(a.y == b.y)
+      return a.x < b.x;
+    return a.y < b.y;
+  }
+  return a.dist < b.dist;
 }
 
 void solution() {
-	while(true) {
-		init();
-		bfs(s.y, s.x);
-		if(v.size() == 0) {
-			cout << s.time << endl;
-			break;
-		}
-		else if(v.size() == 1) 
-			eatFish();			
-		else {
-			sort(v.begin(), v.end(), cmp);
-			eatFish();
-		}
-	}
+  while(true) {
+    init();
+    bfs();
+    if(Fishes.size() == 0) {
+      cout << Shark.time << endl;
+      break;
+    }
+    else if(Fishes.size() == 1) eatFish();
+    else if(Fishes.size() >= 2) {
+      sort(Fishes.begin(), Fishes.end(), cmp);
+      eatFish();
+    }
+  }
 }
 
 void solve() {
-	input();
-	solution();
+  input();
+  solution();
 }
 
 int main() {
-	solve();
-	return 0;
+  solve();
+  return 0;
 }
