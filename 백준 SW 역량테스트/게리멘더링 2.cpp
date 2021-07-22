8f61adffd2acf7ebd005d9e476cd5a0b19414253
inpyeong <58331907+inpyeong@users.noreply.github.com>
2021-07-23 08:19:35 +0900

    Update 게리멘더링 2.cpp

--- 백준 SW 역량테스트/게리멘더링 2.cpp
+++ 백준 SW 역량테스트/게리멘더링 2.cpp
@@ -5,110 +5,93 @@ using namespace std;
 
 const int MAX = 20, INF = 987654321;
 
-struct Coord {
-	int y, x;
-};
-
-int n, answer = INF;
-int m[MAX][MAX];
-int label[MAX][MAX];
-Coord pos[4];
+int N, Answer = INF;
+int Board[MAX][MAX];
+int Label[MAX][MAX];
+pair<int, int> Pos[4];
 
 void input() {
-	cin >> n;
-	for(int i = 0; i < n; ++i)
-		for(int j = 0; j < n; ++j)
-			cin >> m[i][j];
-}
-
-bool isOk(int y, int x, int d1, int d2) {
-	if(y+d1 >= n || x-d1 < 0) 
-		return false;
-	if(y+d2 >= n || x+d2 >= n)
-		return false;
-	if(y+d1+d2 >= n || x-d1+d2 < 0 || x-d1+d2 >= n)
-		return false;
-	return true;
+  cin >> N;
+  for(int i = 0; i < N; ++i)
+    for(int j = 0; j < N; ++j)
+      cin >> Board[i][j];
 }
 
 void printLabel() {
-	cout << "===========" << endl;
-	for(int i = 0; i < n; ++i) {
-		for(int j = 0; j < n; ++j)
-			cout << label[i][j] << " ";
-		cout << endl;
-	}
-	cout << "===========" << endl;
+  cout << "=============" << endl;
+  for(int i = 0; i < N; ++i) {
+    for(int j = 0; j < N; ++j)
+      cout << Label[i][j] << " ";
+    cout << endl;
+  }
+}
+
+bool check(int y, int x, int d1, int d2) {
+  if(y + d1 >= N || x - d1 < 0) return false;
+  if(y + d2 >= N || x + d2 >= N) return false;
+  if(y + d1 + d2 >= N || x - d1 +d2 < 0 || x - d1 + d2 >= N) return false;
+  return true;
 }
 
 void calc() {
-	int sum[6] = {0,};
-	for(int i = 0; i < n; ++i) 
-		for(int j = 0; j < n; ++j) 
-			sum[label[i][j]] += m[i][j];
-	sort(sum, sum+6);
-	int diff = sum[5] - sum[1];
-	answer = min(answer, diff);
+  int sum[6] = {0,};
+  for(int i = 0; i < N; ++i)
+    for(int j = 0; j < N; ++j)
+      sum[Label[i][j]] += Board[i][j];
+  sort(sum, sum+6);
+  Answer = min(Answer, sum[5]-sum[1]);
 }
 
 void labeling() {
-	for(int i = 0; i < n; ++i)
-		for(int j = 0; j < n; ++j)
-			label[i][j] = 5;
-	int sub = 0, plus = 0;
-	// 1
-	for(int i = 0; i < pos[1].y; ++i) {
-		if(i >= pos[0].y)
-			sub++;
-		for(int j = 0; j <= pos[0].x-sub; ++j)
-			label[i][j] = 1;
-	}
-	// 2
-	for(int i = 0; i <= pos[2].y; ++i) {
-		if(i > pos[0].y) plus++;
-		for(int j = pos[0].x+1+plus; j < n; ++j)
-			label[i][j] = 2;
-	}
-	// 3
-	sub = 0;
-	for(int i = n-1; i >= pos[1].y; --i) {
-		if(i < pos[3].y)
-			sub++;
-		for(int j = 0; j < pos[3].x-sub; ++j)
-			label[i][j] = 3;
-	}
-	// 4
-	plus = 0;
-	for(int i = n-1; i > pos[2].y; --i) {
-		if(i <= pos[3].y) plus++;
-		for(int j = pos[3].x+plus; j < n; ++j)
-			label[i][j] = 4;
-	}
-	// printLabel();
-	calc();
+  for(int i = 0; i < N; ++i)
+    for(int j = 0; j < N; ++j)
+      Label[i][j] = 5;
+  int sub = 0, plus = 0;
+  for(int i = 0; i < Pos[1].first; ++i) {
+    if(i >= Pos[0].first) sub++;
+    for(int j = 0; j <= Pos[0].second-sub; ++j)
+      Label[i][j] = 1;
+  }
+  for(int i = 0; i <= Pos[2].first; ++i) {
+    if(i > Pos[0].first) plus++;
+    for(int j = Pos[0].second+1+plus; j < N; ++j)
+      Label[i][j] = 2;
+  }
+  sub = 0, plus = 0;
+  for(int i = N-1; i >= Pos[1].first; --i) {
+    if(i < Pos[3].first) sub++;
+    for(int j = 0; j < Pos[3].second-sub; ++j)
+      Label[i][j] = 3;
+  }
+  for(int i = N-1; i > Pos[2].first; --i) {
+    if(i <= Pos[3].first) plus++;
+    for(int j = Pos[3].second+plus; j < N; ++j)
+      Label[i][j] = 4;
+  }
+  calc();
 }
 
 void solution() {
-	for(int i = 0; i < n; ++i) 
-		for(int j = 0; j < n; ++j) 
-			for(int d1 = 1; d1 <= j; ++d1) 
-				for(int d2 = 1; d2 < n - j; ++d2) 
-					if(isOk(i, j, d1, d2)) {
-						pos[0].y = i, pos[0].x = j;
-						pos[1].y = i+d1, pos[1].x = j-d1;
-						pos[2].y = i+d2, pos[2].x = j+d2;
-						pos[3].y = i+d1+d2, pos[3].x = j-d1+d2;
-						labeling();
-					}
-	cout << answer << endl;
+  for(int i = 0; i < N; ++i)
+    for(int j = 0; j < N; ++j)
+      for(int d1 = 1; d1 <= j; ++d1)
+        for(int d2 = 1; d2 < N-j; ++d2) 
+          if(check(i, j, d1, d2)) {
+            Pos[0].first = i, Pos[0].second = j;
+            Pos[1].first = i + d1, Pos[1].second = j - d1;
+            Pos[2].first = i + d2, Pos[2].second = j + d2;
+            Pos[3].first = i + d1 + d2, Pos[3].second = j - d1 + d2;
+            labeling();
+          }
+  cout << Answer << endl;
 }
 
 void solve() {
-	input();
-	solution();
+  input();
+  solution();
 }
 
 int main() {
-	solve();
-	return 0;
+  solve();
+  return 0;
 }
